20e450f1f908627f682675fe4db9e69d07098c4f
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-05-18 09:41:42 +0200

    navbar mas pages

--- T37RK/src/app/app.module.ts
+++ T37RK/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonajeComponent } from './personaje/personaje.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonajeComponent
+    PersonajeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
